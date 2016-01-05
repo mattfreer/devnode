e00da8f10ab77b1c1b91d8464b0fac7bef45af1d
matt.freer <matt.freer@gmail.com>
2016-01-05 14:04:35 +0000

    feat(router): support routes that match on multiple arguments
    Previously the Router supported routes matched on one argument e.g:
    
    ```bash
    devnode build
    ```
    
    The Router now supports routes that match on multiple arguments. For
    example:
    
    ```bash
    devnode build foo
    devnode build bar
    ```
    
    This commit lays the foundation for future features that will require
    matching on multiple arguments.

--- apps/devnode_client/lib/router.ex
+++ apps/devnode_client/lib/router.ex
@@ -7,19 +7,29 @@ defmodule Devnode.Client.Router do
 
   use Towel
 
-  def execute(options) do
-    f = elem(options, 1) |> match
-    f.(elem(options, 0))
+  def execute({parsed_values, argv, _errors}) do
+    f = match(argv)
+    f.(parsed_values)
   end
 
-  def match(list) do
+  defp match(list) do
     cond do
-      member?(list, "list") -> &ListRoute.execute/1
-      member?(list, "build") -> &BuildRoute.execute/1
-      member?(list, "build-registry") -> &BuildRegistryRoute.execute/1
+      contains?(list, "list") -> &ListRoute.execute/1
+      contains?(list, "build") -> &BuildRoute.execute/1
+      contains?(list, "build-registry") -> &BuildRegistryRoute.execute/1
 
       true ->
         fn(_values) -> error("no match") end
     end
+  end
+
+  defp contains?(argv, target) when is_list(target) do
+    Enum.all?(target, fn(t) ->
+      Enum.member?(argv, t)
+    end)
+  end
+
+  defp contains?(argv, target) do
+    contains?(argv, [target])
   end
 end
